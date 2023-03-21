d3ce5c1ba9b43200c625e76a84412944254e909b
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-03-21 22:07:31 +0500

    pushed

--- script.js
+++ script.js
@@ -20,6 +20,19 @@ function init() {
   document.getElementById('msg').appendChild(res_elm);
 }
 
+var input = document.getElementById("msg_send");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+
+    document.getElementById("reply").click();
+  }
+});
 
 document.getElementById('reply').addEventListener("click", async (e) => {
   e.preventDefault();
